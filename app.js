cee1e3903ad30b519fa9b05be3571a14cd9458e6
dragonthakur2107 <hemanshsinghthakur@gmail.com>
2021-08-28 11:58:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function getTranslationURL(text){
 
 function errorHandler(error){
      console.log("error occured",error);
-     alert("Something went wrong")
+     alert("Something went wrong");
 } 
 
 function dothis(){
